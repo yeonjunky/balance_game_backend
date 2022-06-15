0e6fe1b7220c5231e81dbe1d461c6b3b1cda2f26
yeonjun <yeonjunky@gmail.com>
2022-06-15 14:10:02 +0900

    add rest_framework.authtoken to INSTALLED_APPS

--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'game',
     'users',
     'rest_framework',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
