83c56a9f97018b75b5976b3bbfe5e8109dded11e
yeonjun <yeonjunky@gmail.com>
2022-06-27 00:51:05 +0900

    remove unused import

--- users/views.py
+++ users/views.py
@@ -2,7 +2,6 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.authtoken.models import Token
-import rest_framework.authtoken.views
 
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
