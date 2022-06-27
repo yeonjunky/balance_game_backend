ddfa96b535cea871b5c0e962d0f9c07f4f2927c3
yeonjun <yeonjunky@gmail.com>
2022-06-27 12:36:30 +0900

    remove unused import

--- game/views.py
+++ game/views.py
@@ -4,8 +4,6 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-import json
-
 from .models import Game
 from .serializers import GameSerializer
 
