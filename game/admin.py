2e19b45c945883692ef673976e5feb1b1b749f58
yeonjun <yeonjunky@gmail.com>
2022-06-27 17:18:04 +0900

    add choice inline, game admin

--- game/admin.py
+++ game/admin.py
@@ -4,5 +4,16 @@ from .models import Game, Choice
 
 
 # Register your models here.
-admin.site.register(Game)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+
+class GameAdmin(admin.ModelAdmin):
+    inlines = [
+        ChoiceInline,
+    ]
+
+
+admin.site.register(Game, GameAdmin)
 admin.site.register(Choice)
