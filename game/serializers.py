c6039b978bf58bf761ca805a88f5613af77404dd
yeonjun <yeonjunky@gmail.com>
2022-06-27 23:09:34 +0900

    created ChoiceSerializer

--- game/serializers.py
+++ game/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from .models import Game
+from .models import Game, Choice
+
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Choice
+        fields = '__all__'
 
 
 class GameSerializer(serializers.ModelSerializer):
+    choices = ChoiceSerializer(source='choice_set', many=True)
+
     class Meta:
         model = Game
         fields = '__all__'
