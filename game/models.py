908409826ecf02b5a8a05cab9c40bfe28f8602df
yeonjun <yeonjunky@gmail.com>
2022-06-27 17:18:45 +0900

    remove fields

--- game/models.py
+++ game/models.py
@@ -5,20 +5,10 @@ from django.core.exceptions import ValidationError
 # Create your models here.
 class Game(models.Model):
     title = models.CharField(max_length=256)
-    choice_1_text = models.CharField(max_length=256)
-    choice_2_text = models.CharField(max_length=256)
-    vote_1 = models.PositiveIntegerField(default=0)
-    vote_2 = models.PositiveIntegerField(default=0)
 
     def clean(self):
         if not self.title:
             raise ValidationError("Title is empty")
-
-        if self.choice_1_text is None or self.choice_2_text is None:
-            raise ValidationError("At least one choice is blank")
-
-        elif self.choice_1_text == self.choice_2_text:
-            raise ValidationError("The choices' text is the same")
 
     def __str__(self):
         return self.title
