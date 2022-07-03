3397d366760752fbaa202c42ecc8579af3168f31
yeonjun <yeonjunky@gmail.com>
2022-07-03 17:48:40 +0900

    add new model Vote

--- game/models.py
+++ game/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Game(models.Model):
     title = models.CharField(max_length=256)
+    voted_users = models.ManyToManyField(User, related_name='voted_users', blank=True)
 
     def clean(self):
         if not self.title:
@@ -17,7 +19,6 @@ class Game(models.Model):
 class Choice(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='choices')
     text = models.CharField(max_length=256)
-    votes = models.PositiveIntegerField(default=0)
 
     def vote(self):
         self.votes += 1
@@ -29,3 +30,9 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.text
+
+
+class Vote(models.Model):
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    users = models.ManyToManyField(User, related_name='users')
+    side = models.BooleanField()  # left side choice : False, right side choice : True
