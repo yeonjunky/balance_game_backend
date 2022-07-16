9a68745aa4c353ee99e8de152589b14b62e8092f
yeonjun <yeonjunky@gmail.com>
2022-07-16 22:14:58 +0900

    add and register vote inline and choice admin

--- game/admin.py
+++ game/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from .models import Game, Choice
+from .models import Game, Choice, Vote
 
 
 # Register your models here.
+class VoteInline(admin.TabularInline):
+    model = Vote
+    extra = 0
+
+
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 0
@@ -15,5 +20,12 @@ class GameAdmin(admin.ModelAdmin):
     ]
 
 
+class ChoiceAdmin(admin.ModelAdmin):
+    inlines = [
+        VoteInline,
+    ]
+
+
 admin.site.register(Game, GameAdmin)
-admin.site.register(Choice)
+admin.site.register(Choice, ChoiceAdmin)
+admin.site.register(Vote)
