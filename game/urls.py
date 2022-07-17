481352ebf91c94fad2536c965e216028fc13bc25
yeonjun <yeonjunky@gmail.com>
2022-07-17 10:53:12 +0900

    add slashes

--- game/urls.py
+++ game/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('games/', views.games),
-    path('games/<int:game_id>', views.games),
-    path('games/<int:game_id>/vote', views.vote),
+    path('games/<int:game_id>/', views.games),
+    path('games/<int:game_id>/vote/', views.vote),
 ]
