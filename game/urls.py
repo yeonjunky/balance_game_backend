aceb05caae3e8a698a451eece4302b9d2c49d8e3
yeonjun <yeonjunky@gmail.com>
2022-08-03 16:34:37 +0900

    add user posted games api path

--- game/urls.py
+++ game/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('games/', views.games),
     path('games/<int:game_id>/', views.games),
     path('games/<int:game_id>/vote/', views.vote),
+    path('games/user/<str:username>/', views.posted_games),
 ]
