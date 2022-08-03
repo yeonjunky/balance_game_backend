cbc1481957180aa9a237ad4540bcc111e6914aca
yeonjun <yeonjunky@gmail.com>
2022-08-03 16:43:07 +0900

    add imports User and PostedGameSerializer for user posted games

--- game/views.py
+++ game/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import get_object_or_404
 from django.core.exceptions import ValidationError
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 from .models import Game
-from .serializers import GameSerializer
+from .serializers import GameSerializer, PostedGameSerializer
 
 
 # Create your views here.
