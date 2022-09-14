9cfbbba4fa38697392362d7d3722f0814e5441c4
yeonjun <yeonjunky@gmail.com>
2022-09-14 21:39:37 +0900

    sign up test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+# from django.contrib.
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+# from rest_framework.authtoken.models import Token
+
 
 # Create your tests here.
+class UserTest(TestCase):
+    # def setUp(self) -> None:
+    #     user = User.objects.create()
+
+    def test_sign_up(self):
+        username = 'asdf'
+        email = 'test@example.com'
+        password = '1234'
+
+        created_user = User.objects.create_user(username=username, email=email, password=password)
+
+        user = User.objects.get(pk=created_user.id)
+
+        self.assertEqual(created_user.username, user.username)
+        self.assertEqual(created_user.email, user.email)
+        self.assertEqual(created_user.password, user.password)
+
