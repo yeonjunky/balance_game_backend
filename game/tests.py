57bc99c7477112fb4992e3d9ddf6bff3b73e79a1
yeonjun <yeonjunky@gmail.com>
2022-09-15 18:49:01 +0900

    game serializer get game test

--- game/tests.py
+++ game/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 
-from .models import Game
+from .models import Game, Choice
 from .serializers import GameSerializer
 
 
@@ -42,7 +42,22 @@ class GameSerializerTest(TestCase):
         self.assertEqual(choice1, game.choices.get(pk=choices[0].id).text)
         self.assertEqual(choice2, game.choices.get(pk=choices[1].id).text)
 
+    def test_get_question(self):
+        title = 'test'
+        choice1 = 'left'
+        choice2 = 'right'
 
+        game = Game.objects.create(title=title, author=self.user)
 
+        c1 = Choice.objects.create(text=choice1, game_id=game.id)
+        c2 = Choice.objects.create(text=choice2, game_id=game.id)
 
+        game.choices.add(c1)
+        game.choices.add(c2)
 
+        serializer = GameSerializer(game)
+
+        data = serializer.data
+        self.assertEqual(title, data.get('title'))
+        self.assertEqual(data.get('choices')[0]['text'], choice1)
+        self.assertEqual(data.get('choices')[1]['text'], choice2)
