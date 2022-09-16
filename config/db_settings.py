7d3689943de4ef6968ff8735185ac389455a6124
yeonjun <yeonjunky@gmail.com>
2022-09-16 09:48:15 +0900

    add test database settings

--- config/db_settings.py
+++ config/db_settings.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -6,5 +8,15 @@ DATABASES = {
         'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '3306',
-    }
+
+        'TEST': {
+            'mirror': 'test'
+
+        },
+
+        'test.sqlite3': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'test.sqlite3'
+        }
+    },
 }
